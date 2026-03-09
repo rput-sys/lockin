eb494b220e6ed0b93a6724ab938716dfd7ba3f52
rputhum4 <rputhum4@umd.edu>
2026-03-09 02:10:56 -0400

    fix usePWA ArrayBuffer type

--- web/src/hooks/usePWA.ts
+++ web/src/hooks/usePWA.ts
@@ -98,9 +98,9 @@ export function usePWA() {
   };
 }
 
-function urlBase64ToUint8Array(base64String: string): Uint8Array {
+function urlBase64ToUint8Array(base64String: string): ArrayBuffer {
   const padding = '='.repeat((4 - base64String.length % 4) % 4);
   const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
   const rawData = window.atob(base64);
-  return Uint8Array.from([...rawData].map(c => c.charCodeAt(0)));
+  return Uint8Array.from([...rawData].map(c => c.charCodeAt(0))).buffer;
 }
